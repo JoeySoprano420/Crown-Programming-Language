e8f20a0d917f27e8add294d9e4dd639855ca3f3f
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-08-22 00:24:43 -0400

    Add compilation commands for executables

--- convert_to_exe.sh
+++ convert_to_exe.sh
@@ -1,3 +1,8 @@
 clang C:\Users\420up\Crown-Programming-Language\crown_runtime.o hello.o -o hello.exe
 
 clang C:\Users\420up\Crown-Programming-Language\crown_runtime.o -o crown_runtime.exe
+
+cl crown_runtime.c /Fo:crown_runtime.obj /Fe:crown_runtime.exe
+
+cl crown_runtime.obj hello.obj /Fe:hello.exe
+
